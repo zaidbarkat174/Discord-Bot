367617e170b5ca4bbaff4c9c3f66d9c18a8f20ac
bernie-zhu <54250363+bernie-zhu@users.noreply.github.com>
2021-01-07 19:19:20 -0600

    Add files via upload
    all kanto routes and pokemon they have w/ encounter chances

---
+++ Info/Areas.py
@@ -0,0 +1,151 @@
+import random
+import pokedex
+route_1_chances = 5, 5
+route_1_pokemon = pokemon_array[15], pokemon_array[18]
+route_1 = [v for v, d in zip(route_1_pokemon, route_1_chances) for i in range(d)]
+for i in range(1):
+    print(random.choice(route_1))
+
+route_2_chances = 15, 45, 40
+route_2_pokemon = pokemon_array[12], pokemon_array[15], pokemon_array[18]
+route_2 = [v for v, d in zip(route_2_pokemon, route_2_chances) for i in range(d)]
+for i in range(1):
+    print(random.choice(route_2))
+
+route_3_chances = 45, 45, 10
+route_3_pokemon = pokemon_array[15], pokemon_array[20], pokemon_array[38]
+route_3 = [v for v, d in zip(route_3_pokemon, route_3_chances) for i in range(d)]
+for i in range(1):
+    print(random.choice(route_3))
+
+route_4_chances = 40, 35, 25
+route_4_pokemon = pokemon_array[18], pokemon_array[20], pokemon_array[22]
+route_4 = [v for v, d in zip(route_4_pokemon, route_4_chances) for i in range(d)]
+for i in range(1):
+    print(random.choice(route_4))
+
+route_5_chances = 40, 35, 25
+route_5_pokemon = pokemon_array[15], pokemon_array[42], pokemon_array[55]
+route_5 = [v for v, d in zip(route_5_pokemon, route_5_chances) for i in range(d)]
+for i in range(1):
+    print(random.choice(route_5))
+
+route_6_chances = 40, 35, 25
+route_6_pokemon = pokemon_array[15], pokemon_array[42], pokemon_array[55]
+route_6 = [v for v, d in zip(route_6_pokemon, route_6_chances) for i in range(d)]
+for i in range(1):
+    print(random.choice(route_6))
+
+route_7_chances = 30, 30, 30, 10
+route_7_pokemon = pokemon_array[15], pokemon_array[42], pokemon_array[55], pokemon_array[57]
+route_7 = [v for v, d in zip(route_7_pokemon, route_7_chances) for i in range(d)]
+for i in range(1):
+    print(random.choice(route_7))
+
+route_8_chances = 35, 20, 25, 20
+route_8_pokemon = pokemon_array[15], pokemon_array[22], pokemon_array[55], pokemon_array[57]
+route_8 = [v for v, d in zip(route_8_pokemon, route_8_chances) for i in range(d)]
+for i in range(1):
+    print(random.choice(route_8))
+
+route_9_chances = 40, 35, 25
+route_9_pokemon = pokemon_array[18], pokemon_array[20], pokemon_array[22]
+route_9 = [v for v, d in zip(route_9_pokemon, route_9_chances) for i in range(d)]
+for i in range(1):
+    print(random.choice(route_9))
+
+route_10_chances = 30, 25, 45
+route_10_pokemon = pokemon_array[20], pokemon_array[22], pokemon_array[99]
+route_10 = [v for v, d in zip(route_10_pokemon, route_10_chances) for i in range(d)]
+for i in range(1):
+    print(random.choice(route_10))
+
+route_11_chances = 35, 40, 25
+route_11_pokemon = pokemon_array[20], pokemon_array[22], pokemon_array[95]
+route_11 = [v for v, d in zip(route_11_pokemon, route_11_chances) for i in range(d)]
+for i in range(1):
+    print(random.choice(route_11))
+
+route_12_chances = 35, 40, 5, 20
+route_12_pokemon = pokemon_array[15], pokemon_array[42], pokemon_array[43], pokemon_array[47]
+route_12 = [v for v, d in zip(route_12_pokemon, route_12_chances) for i in range(d)]
+for i in range(1):
+    print(random.choice(route_12))
+
+route_13_chances = 35, 40, 5, 20
+route_13_pokemon = pokemon_array[15], pokemon_array[42], pokemon_array[43], pokemon_array[47]
+route_13 = [v for v, d in zip(route_13_pokemon, route_13_chances) for i in range(d)]
+for i in range(1):
+    print(random.choice(route_13))
+
+route_14_chances = 15, 5, 40, 5, 20, 15
+route_14_pokemon = pokemon_array[15], pokemon_array[16], pokemon_array[42], pokemon_array[43], pokemon_array[47], pokemon_array[131]
+route_14 = [v for v, d in zip(route_14_pokemon, route_14_chances) for i in range(d)]
+for i in range(1):
+    print(random.choice(route_14))
+
+route_15_chances = 15, 5, 40, 5, 20, 15
+route_15_pokemon = pokemon_array[15], pokemon_array[16], pokemon_array[42], pokemon_array[43], pokemon_array[47], pokemon_array[131]
+route_15 = [v for v, d in zip(route_15_pokemon, route_15_chances) for i in range(d)]
+for i in range(1):
+    print(random.choice(route_15))
+
+route_16_chances = 30, 5, 40, 25
+route_16_pokemon = pokemon_array[18], pokemon_array[19], pokemon_array[20], pokemon_array[83]
+route_16 = [v for v, d in zip(route_16_pokemon, route_16_chances) for i in range(d)]
+for i in range(1):
+    print(random.choice(route_16))
+
+route_17_chances = 30, 40, 5, 25
+route_17_pokemon = pokemon_array[19], pokemon_array[20], pokemon_array[21], pokemon_array[83]
+route_17 = [v for v, d in zip(route_17_pokemon, route_17_chances) for i in range(d)]
+for i in range(1):
+    print(random.choice(route_17))
+
+route_18_chances = 20, 40, 15, 25
+route_18_pokemon = pokemon_array[19], pokemon_array[20], pokemon_array[21], pokemon_array[83]
+route_18 = [v for v, d in zip(route_18_pokemon, route_18_chances) for i in range(d)]
+for i in range(1):
+    print(random.choice(route_18))
+
+route_19_chances = 1
+route_19_pokemon = pokemon_array[71]
+route_19 = [v for v, d in zip(route_19_pokemon, route_19_chances) for i in range(d)]
+for i in range(1):
+    print(random.choice(route_19))
+
+route_20_chances = 1
+route_20_pokemon = pokemon_array[71]
+route_20 = [v for v, d in zip(route_20_pokemon, route_20_chances) for i in range(d)]
+for i in range(1):
+    print(random.choice(route_20))
+
+route_21_chances = 25, 15, 35, 15, 10
+route_21_pokemon = pokemon_array[15], pokemon_array[16], pokemon_array[18], pokemon_array[19], pokemon_array[113]
+route_21 = [v for v, d in zip(route_21_pokemon, route_21_chances) for i in range(d)]
+for i in range(1):
+    print(random.choice(route_21))
+
+route_22_chances = 45, 10, 5, 40
+route_22_pokemon = pokemon_array[18], pokemon_array[20], pokemon_array[28], pokemon_array[31]
+route_22 = [v for v, d in zip(route_22_pokemon, route_22_chances) for i in range(d)]
+for i in range(1):
+    print(random.choice(route_22))
+
+route_23_chances = 15, 25, 25, 5, 30
+route_23_pokemon = pokemon_array[20], pokemon_array[21], pokemon_array[22], pokemon_array[33], pokemon_array[131]
+route_23 = [v for v, d in zip(route_23_pokemon, route_23_chances) for i in range(d)]
+for i in range(1):
+    print(random.choice(route_23))
+
+route_24_chances = 20, 20, 20, 25, 15
+route_24_pokemon = pokemon_array[12], pokemon_array[13], pokemon_array[15], pokemon_array[42], pokemon_array[62]
+route_24 = [v for v, d in zip(route_24_pokemon, route_24_chances) for i in range(d)]
+for i in range(1):
+    print(random.choice(route_24))
+
+route_25_chances = 1, 4, 20, 20, 15, 25, 15
+route_25_pokemon = pokemon_array[9], pokemon_array[10], pokemon_array[12], pokemon_array[13], pokemon_array[15], pokemon_array[42], pokemon_array[62]
+route_25 = [v for v, d in zip(route_25_pokemon, route_25_chances) for i in range(d)]
+for i in range(1):
+    print(random.choice(route_25))
